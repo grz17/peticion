cf2ca08622b8e720007abad6dab3a09696682516
grz17 <resendizgabriel19@gmail.com>
2021-06-16 15:04:07 -0500

    segunda peticion

--- src/app/pages/sumanumeros/sumanumeros.component.ts
+++ src/app/pages/sumanumeros/sumanumeros.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {DataService} from '../../servicios/data.service';
 @Component({
   selector: 'app-sumanumeros',
   templateUrl: './sumanumeros.component.html',
   styleUrls: ['./sumanumeros.component.css']
 })
 export class SumanumerosComponent implements OnInit {
-
-  constructor() { }
+  mensajes:any[]=[];
+  numero: number=0;
+  numero1:number=0;
+  constructor( private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getSuma(this.numero,this.numero1).subscribe( (post:any)=> {
+      this.mensajes=post.valor;
+      console.log(post);
+    });
+    
   }
 
 }
--- src/app/servicios/data.service.ts
+++ src/app/servicios/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { SumanumerosComponent } from '../pages/sumanumeros/sumanumeros.component';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,11 @@ export class DataService {
   
   getPost(){
     return this.http.get('https://hostcjcafe.azurewebsites.net/Servicios/Hola.php');
+  }
+
+  getSuma(numero1:number,numero: number)
+  {
+  
+    return this.http.get('https://hostcjcafe.azurewebsites.net/Servicios/Suma.php?num1='+numero+'&num2='+numero1+'');
   }
 }
